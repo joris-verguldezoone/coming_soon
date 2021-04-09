b64403613f16a07cc6306faa8f7dcef607310fad
joris-verguldezoone <joris.verguldezoone@laplateforme.io>
2021-04-09 17:46:23 +0200

    v1 responsive

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coming Soon | Zill'Event by Léa Zillweger</title>
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Public+Sans&display=swap" rel="stylesheet"> 
+    <link rel='stylesheet' href='style.css'>
+</head>
+<body>
+    <main>
+    
+        <article id='main_row'>
+            <aside>
+                <h1>COMING</h1>
+                <h2>SOON</h2>
+            </aside>
+            <div id="photoCouv"></div>
+        </article>
+       
+        
+        <article id='article_infos'>
+            <section id="section_name">
+        
+                <h3>Zill'Event by<br>Léa Zillweger</h3>
+          
+            </section>
+            <section id="section_infos">
+            <div class="hide">
+                <ul>
+                    <li>Agence</li>
+                    <li>Événement</li>
+                    <li>Institutionnel</li>
+                    <li>Communication</li>
+                </ul>
+            </div>
+
+            <div>
+                <a href="mailto:lea@zill-event.com">lea@zill-event.com</a>
+                <p>07 69 66 02 49</p>
+            </div>
+
+            <div>
+                <ul>
+                <li><a target="blank" href="https://www.facebook.com/lea.zillweger.94/">Facebook</a></li>
+                <li><a target="blank" href="https://www.instagram.com/leazillweger/">Instagram</a></li>
+                <li><a target="blank" href="https://www.linkedin.com/in/leazillweger/">Linkedin</a></li>
+                </ul>
+            </div>
+
+            <div  class="hide">
+                <p>Paris & the world</p>
+                <p>Marseille</p>
+            </div>
+            </section>
+        </article>
+
+    </main>
+</body>
+</html>
