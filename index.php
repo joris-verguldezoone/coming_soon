9a5a351f5a376b48247f11dad477f35423e0be92
huongmay-nguyenphuoc <71871990+huongmay-nguyenphuoc@users.noreply.github.com>
2021-04-11 18:03:46 +0200

    Responsive retravaillé
    - réduction du code, refactorisation
    - cross browser

--- index.php
+++ index.php
@@ -4,60 +4,69 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Cheffe de projet événementielle basée à Marseille. Forte d'une expérience en gestion d'événements physiques & digitaux,
+     je vous propose mes services en vous accompagnant dans la réalisation et la réussite de vos évents.">
     <title>Coming Soon | Zill'Event by Léa Zillweger</title>
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+          integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Public+Sans&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css2?family=Public+Sans&display=swap" rel="stylesheet">
     <link rel='stylesheet' href='style.css'>
 </head>
+
 <body>
-    <main>
-    
-        <article id='main_row'>
-            <aside>
-                <h1>COMING</h1>
-                <h2>SOON</h2>
-            </aside>
-            <div id="photoCouv"></div>
-        </article>
-       
-        
-        <article id='article_infos'>
-            <section id="section_name">
-        
-                <h3>Zill'Event by<br>Léa Zillweger</h3>
-          
-            </section>
-            <section id="section_infos">
+<main>
+
+    <article id='main_row'>
+        <aside>
+            <span>
+            <h1>COMING</h1>
+            <h2>SOON</h2>
+            </span>
+        </aside>
+        <div id="photoCouv"></div>
+    </article>
+
+
+    <article id='article_infos'>
+        <section id="section_name">
+            <h3>Zill'Event <br>by Léa Zillweger</h3>
+            <div id="show">
+                <a href="mailto:lea@zill-event.com">lea@zill-event.com</a>
+                <p>07 69 66 02 49</p>
+            </div>
+        </section>
+        <section id="section_infos">
             <div class="hide">
                 <ul>
-                    <li>Agence</li>
                     <li>Événement</li>
+                    <li>Entreprise</li>
                     <li>Institutionnel</li>
                     <li>Communication</li>
+                    <li>Management</li>
                 </ul>
             </div>
 
-            <div>
+            <div class="hide">
                 <a href="mailto:lea@zill-event.com">lea@zill-event.com</a>
                 <p>07 69 66 02 49</p>
             </div>
 
             <div>
                 <ul>
-                <li><a target="blank" href="https://www.facebook.com/lea.zillweger.94/">Facebook</a></li>
-                <li><a target="blank" href="https://www.instagram.com/leazillweger/">Instagram</a></li>
-                <li><a target="blank" href="https://www.linkedin.com/in/leazillweger/">Linkedin</a></li>
+                    <li><a target="blank" href="https://www.facebook.com/lea.zillweger.94/">Facebook</a></li>
+                    <li><a target="blank" href="https://www.instagram.com/leazillweger/">Instagram</a></li>
+                    <li><a target="blank" href="https://www.linkedin.com/in/leazillweger/">Linkedin</a></li>
                 </ul>
             </div>
 
-            <div  class="hide">
+            <div class="hide">
                 <p>Paris & the world</p>
                 <p>Marseille</p>
             </div>
-            </section>
-        </article>
+        </section>
+    </article>
 
-    </main>
+</main>
 </body>
 </html>
